97d1789f0db94f5cae475c58207d099013fec1fa
Nekwan Luke <contact@nekwanluke.com>
2019-03-05 01:16:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,12 +19,12 @@ function sayHiToGrandma(string)
   if (string.toLowerCase() === string) {
     return ("I can't hear you!")
   }
-}
 
-function sayHiToGrandma(string)
-{
   if (string.toUpperCase() === string) {
     return ("YES, INDEED!")
+  }
+  if (string === "I love you, Grandma.") {
+    return ("I love you, too.")
   }
 }
 
